68e803113c6a9a705691c2469dd6547977bd9fb1
Thomas Ellis <thomas.ellis@kaltura.com>
2020-11-22 21:29:27 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
 alert("loading JS...");
-
-kWidget.addReadyCallback(playerId => {
-  let kdp = document.getElementById(playerId);
-  kdp.kBind("playerReady", function() {
-    console.log("Kaltura player is ready");
-  });
-});
