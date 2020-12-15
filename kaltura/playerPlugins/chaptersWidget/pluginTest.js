f800191d441f3673cd053532d5f579269db49715
Thomas Ellis <thomas.ellis@kaltura.com>
2020-12-15 08:41:56 -0500

    updating ui

--- kaltura/playerPlugins/chaptersWidget/pluginTest.js
+++ kaltura/playerPlugins/chaptersWidget/pluginTest.js
@@ -43,10 +43,12 @@ mw.kalturaPluginWrapper(function(){
 					console.log("adding chapters container...");
 		       		_this.getChaptersWidgetContainer();
 		       		console.log("adding chapters to widget...");
-		       		//$('.chaptersInterface').append('<div id="chaptersList"></div>').css("background-color":"#eaeaea");
 					$.each(data.objects, function (index, chapter) {
 						console.log(chapter.title);
-						$('#chaptersList').append(' ', $('<button>').addClass('chapterButtons').attr('timestamp', chapter.startTime).html(chapter.title).on('click', function(e) {_this.getPlayer().sendNotification("doSeek", Math.floor((chapter.startTime)/1000));}));
+						var seconds = Math.floor((chapter.startTime)/1000);
+						var timestamp = new Date(seconds * 1000).toISOString().substr(11, 8);
+						//$('#chaptersList').append(' ', $('<button>').addClass('chapterButtons').attr('timestamp', chapter.startTime).html(chapter.title).on('click', function(e) {_this.getPlayer().sendNotification("doSeek", Math.floor((chapter.startTime)/1000));}));
+						$('#chaptersList').append('<div class="chapterLink"><div class="chapterItem chapterTime"><span>' + timestamp + '</span></div><div class="chapterItem chapterTitle">' + chapter.title + '</div></div>').on('click', function(e) {_this.getPlayer().sendNotification("doSeek", seconds);});
 					});					
 				}				
             });
