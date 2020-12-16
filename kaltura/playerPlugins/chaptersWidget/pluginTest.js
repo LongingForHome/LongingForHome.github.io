bb95ca7e991a0e0ef08032dffaa640c4f0985eda
Thomas Ellis <thomas.ellis@kaltura.com>
2020-12-16 02:50:16 -0500

    Update pluginTest.js

--- kaltura/playerPlugins/chaptersWidget/pluginTest.js
+++ kaltura/playerPlugins/chaptersWidget/pluginTest.js
@@ -58,7 +58,8 @@ mw.kalturaPluginWrapper(function(){
 					console.log("No chapters available.  Resetting player height to " + resetHeight);
 					$( ".mwPlayerContainer" ).css( "height", resetHeight );
 					// hack to force page redraw of the new element height
-					$( ".mwPlayerContainer" ).hide().show();
+					$( ".mwPlayerContainer" ).hide();
+					$( ".mwPlayerContainer" ).show();
 				}				
             });
         },
