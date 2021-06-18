55d0e4e1ff109eff956c7ad5b13eaf3ea947469a
Thomas Ellis <thomas.ellis@kaltura.com>
2021-06-18 09:45:27 -0400

    Update h5pLink.js

--- kaltura/playerPlugins/h5pLink/h5pLink.js
+++ kaltura/playerPlugins/h5pLink/h5pLink.js
@@ -40,7 +40,7 @@ mw.kalturaPluginWrapper(function(){
         	// get the container to insert the button into
         	console.log(_this.getConfig('downloadLink')); 
         	//$ (".topBarContainer").append("<button class=\"btn pull-right\" ><img src=\"https://longingforhome.github.io/kaltura/playerPlugins/h5pLink/h5p_logo.png\"></button>"); 
-        	$ (".topBarContainer").append(' ', $("<button class=\"btn pull-right\" ><img src=\"https://longingforhome.github.io/kaltura/playerPlugins/h5pLink/h5p_logo.png\"></button>").on('click', function(e) {alert("Use this link in H5P: " + _this.getConfig('downloadLink')}));     	
+        	$ (".topBarContainer").append(' ', $("<button class=\"btn pull-right\" ><img src=\"https://longingforhome.github.io/kaltura/playerPlugins/h5pLink/h5p_logo.png\"></button>").on('click', function(e) {alert("Use this link in H5P: " + _this.getConfig('downloadLink'))}));     	
         	//$ (".topBarContainer").append("<button class=\"btn pull-right\" ><img src=\"" + _this.getConfig('downloadLink') + "\"></button>");
         }
 	}));
