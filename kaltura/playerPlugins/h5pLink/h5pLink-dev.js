2a5cb01ec480243558643714f4f9432e664f05f5
Thomas Ellis <thomas.ellis@kaltura.com>
2021-06-21 11:14:31 -0400

    dev option

--- kaltura/playerPlugins/h5pLink/h5pLink-dev.js
+++ kaltura/playerPlugins/h5pLink/h5pLink-dev.js
@@ -30,6 +30,7 @@ mw.kalturaPluginWrapper(function(){
                 'action' : 'get',
                 'entryId' : this.getPlayer().kentryid                		
             }, function( data ) {                
+				//alert("Use this link in H5P: " + data.downloadUrl);	
 				_this.setConfig('downloadLink', data.downloadUrl);			
             });
         },
@@ -37,7 +38,11 @@ mw.kalturaPluginWrapper(function(){
         	console.log("constructButton called");
         	var _this = this;
         	// get the container to insert the button into
-        	$ (".topBarContainer").append(' ', $("<button class=\"btn pull-right\" ><img src=\"https://longingforhome.github.io/kaltura/playerPlugins/h5pLink/h5p_logo.png\"></button>").on('click', function(e) {alert("Use this link in H5P:\n " + _this.getConfig('downloadLink'))}));
+        	//console.log(_this.getConfig('downloadLink')); 
+        	//$ (".topBarContainer").append("<button class=\"btn pull-right\" ><img src=\"https://longingforhome.github.io/kaltura/playerPlugins/h5pLink/h5p_logo.png\"></button>"); 
+        	$ (".topBarContainer").append(' ', $("<button class=\"btn pull-right\" ><img src=\"https://longingforhome.github.io/kaltura/playerPlugins/h5pLink/h5p_logo.png\"></button>").on('click', function(e) {alert("Use this link in H5P:\n " + _this.getConfig('downloadLink'))}));     	
+        	//$ (".topBarContainer").append("<button class=\"btn pull-right\" ><img src=\"" + _this.getConfig('downloadLink') + "\"></button>");
+        	//$ (" .mwEmbedPLayer").append('', $("<div id=\"h5pModal\" class=\"h5pModal\"><div class=\"h5pModalContent\"><span class=\"closeModal\">&times;</span><p>Use the following link in your H5P project:</p><p>" + _this.getConfig('downloadLink') + "</p></div></div>"));
         }
 	}));
 });
