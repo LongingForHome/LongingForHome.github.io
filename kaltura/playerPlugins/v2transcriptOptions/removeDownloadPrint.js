670f1411cb0deefc57f8cf5adf8008949eb42ffd
Thomas Ellis <thomas.ellis@kaltura.com>
2021-07-12 14:55:44 -0400

    Update removeDownloadPrint.js

--- kaltura/playerPlugins/v2transcriptOptions/removeDownloadPrint.js
+++ kaltura/playerPlugins/v2transcriptOptions/removeDownloadPrint.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	// remove the print icon element
-	$('.transcript-menu-item.printWrapper').remove();
+	$('.printWrapper').remove();
 	console.log("removing transcript print icon...");
 	// remove the download icon element
-	$('.transcript-menu-item.downloadWrapper').remove();
+	$('.downloadWrapper').remove();
 	console.log("removing transcript download icon...");
 });
 
